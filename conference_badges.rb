2e1ad6ccb95ba10aa51f8abc3ae70304e976b03a
Roberto Livi <robertomlivi@outlook.com>
2020-01-23 04:07:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,27 +1,11 @@
 def badge_maker(name)
-  "Hello, my name is #{name}."
+  "Hello, my name is #{name}"
 end
 
-
-def batch_badge_creator(attendees)
-  attendees.map do |names|
-    badge_maker(names)
-  end
+def batch_badge_maker()
+    new_array = []
+    names.each do |name|
+      new_array.push(badge_maker(name))
+    end
+    return new_array
 end
-
-def room_assignments()
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  room = 1
-  attendees.map do |names|
-    puts "Hello #{names}! You'll be assigned to room #{room}"
-    room += 1
-  end
-end
-
-def printer()
-  #batch_badge_creator()
-  room_assignments()
-end
-
-
-printer()
