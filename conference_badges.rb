b557f68abe8f6d905a408466d1bf8547a4bb560d
Roberto Livi <robertomlivi@outlook.com>
2020-01-23 04:11:09 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}"
 end
 
-def batch_badge_maker()
-    new_array = []
-    names.each do |name|
-      new_array.push(badge_maker(name))
+def batch_badge_maker(attendees)
+    attendees.map do |name|
+      badge_maker(name)
     end
-    return new_array
 end
