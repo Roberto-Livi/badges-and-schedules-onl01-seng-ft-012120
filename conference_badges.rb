91ba7ff48c260a4121a05759516d0f43acf03313
Roberto Livi <robertomlivi@outlook.com>
2020-01-23 04:21:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,9 @@ def batch_badge_maker(attendees)
       badge_maker(name)
     end
 end
+
+def assign_rooms(speakers)
+  speakers.map.with_index do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index + 1}"
+  end
+end
